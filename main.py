dca24fdeaac92e5d81e97a378e79b2ef2e52dd5a
Artur Mendes <54417108+Odamota@users.noreply.github.com>
2021-11-28 15:58:11 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,56 +1,144 @@
-#!/usr/bin/python3
-#Author: Artur Mendes @ LESI PL 14870
+#Authored by Artur Mendes nº14870
 
-#from csvReader import csvReader
-from my_lib import slurp
 import ply.lex as plex
-
-#Contents  = csvReader()
-
-
-tokens = ("DESCRIPTIONLINE", "COMMENT", "CONTENT", "COUNTRYNAME", "CAPITAL", "CURRENCY", "OFFICIALLANGUAGE", "HEADOFGOVERNMENT")
-global mycolcounter
-
-def t_DESCRIPTIONLINE(t):
-    r"""Country [A-Z].*\n"""
-    print(f"Apanha 1ª linha:-> {t.value}\n")
-    pass
-
-def t_COMMENT(t):
-    r"""\#[a-z]|[A-Z]"""
-    #this indicates that the line should not be counted or saved
-    #following # there must be always a letter
-    pass
-
-def t_CONTENT(t):
-    r"""[A-Z][a-z]+.*\,"""
-    global mycolcounter
-    mycolcounter = mycolcounter+1
-
-    print(f"Catching line 1 -{mycolcounter}->{t.value}\n")
-    pass
-
-def t_COUNTRYNAME(t):
-    r"""([A-Z][a-z]+),"""
-    #([A-Z][a-z]+(( )?)(([A-Z])?[a-z]*)?),
-    #Inicia por maiusculas, contem letras e 
-    #da currencies e capital + contry name
-    print(f"Nome do Pais->{t.value}\n")
-    pass
-
-def t_HEADOFGOVERNMENT(t):
-    r"""(,[A-Z][a-z]+.*)\n"""
-    print(f"Nome presidente->{t.value}\n")
-    pass
-    
-
-def t_error(t):
-    print(f"Token inesasperado - erro: {t.value}\n")
-    exit(1)
+from ply.lex import TOKEN
+from my_lib import slurp
+#from htmlreport import htmlreport
 
 
+tokens = ( "VALUES", "NEWLINE")
+
+
+t_ANY_ignore = ""
+
+global linecounter
+
+linecounter =0 
+
+
+headings = []
+values = []
+
+
+def t_VALUES(t):
+    r"""(,||^)([^",\n\r]+|"(?:[^"]|"")*")"""
+    global linecounter
+    Value = t.value.replace(",", "")
+    if(linecounter < 1):
+        headings.append(Value)
+    else:
+        values.append(Value)
+    return t
+
+
+def t_NEWLINE(t):
+    r"\n"
+    global linecounter
+    linecounter = linecounter +1
+    t.lexer.begin("INITIAL")
+
+
+def t_ANY_error(t):
+    print(f"Unexpected Token Error ->{t.value}")
+    return t
+
+#lexer initialization
 lexer = plex.lex()
-lexer.input(slurp("sampleFile.txt"))
-lexer.token()
-##print(csvReader.Top())
-print("teste1") 
+#lexer.input(slurp("sample.txt"))
+readingfile = input("Escolha o ficheiro que pretende analisar: \n(sample.txt existe no diretório para testes)\n")
+lexer.input(slurp(readingfile))
+
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break #no more input
+    print(tok)
+
+
+
+global table_content
+global table_header
+
+text = '''
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+'''
+
+heading = '<h2>Relatório de Dados do ficheiro </h2>'
+table_content_heading = '<table>  <tr>'
+
+
+
+print(headings)
+print(values)
+print(f"\nforam processadas cerca de {linecounter} linhas.\n")
+
+#lexer.input(numer of lines to report)
+
+print(f"1-{linecounter}")
+linestoReport = input("Escolha o nº de linhas que pretende adicionar ao relatório : \n")
+#lexer.input(slurp(linestoReport))
+
+file = open("report.html","w+")
+file.write(text)
+file.write(heading)
+file.write(table_content_heading)
+
+for heading in headings:
+   table_header = '<th>'+heading+'</th>'
+   file.write(table_header)
+
+table_closing_heading = '  </tr>'
+file.write(table_closing_heading)
+#contents
+table_content_open = '<tr> '
+file.write(table_content_open)
+table_content_close = '  </tr>'
+counter = 0
+
+
+for value in values:
+        if(counter < int(linestoReport)):    
+            table_content =  '<td>'+value+'</td>'
+            counter = counter +1 
+            if(counter <= len(headings)):
+                    file.write(table_content)
+            else:
+                
+                    file.write(table_content_close)
+                    file.write(table_content_open)
+                    file.write(table_content)
+                    counter = 1
+
+
+file.write(table_content_close)
+table_close = '</table>'
+file.write(table_close)
+html_close= '</body> </html>'
+file.write(html_close)
+file.close()
+
+
+
+print(f"\nComprimento de colunas {len(headings)} .\n")
